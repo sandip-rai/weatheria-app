1394d746cb221cf42c745b26b38bc9b604853430
Sandip Rai <sandip.sampanG@gmail.com>
2018-05-11 17:42:28 -0500

    Added three classes: Day, Forecast, Hour for displaying more data

---
+++ app/src/main/java/com/sandiprai/weatheria/weather/Forecast.java
@@ -0,0 +1,11 @@
+package com.sandiprai.weatheria;
+
+/**
+ * Created by inora on 5/11/2018.
+ */
+
+public class Forecast {
+    private CurrentWeather currentWeather;
+    private Hour[] hourlyForecast;
+    private Day[] dailyForecast;
+}
---
+++ app/src/main/java/com/sandiprai/weatheria/weather/Hour.java
@@ -0,0 +1,53 @@
+package com.sandiprai.weatheria;
+
+/**
+ * Created by Sandip on 5/11/2018.
+ */
+
+public class Hour {
+    private long time;
+    private String summary;
+    private double temp;
+    private String icon;
+    private String timeZone;
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+}
